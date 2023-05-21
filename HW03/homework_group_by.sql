f21b3e47dd9dab5691e6ed59b8fb584db3f281ed
sorokin-va <joker.sva@gmail.com>
2023-05-21 10:52:33 +0300

    new update

---
+++ HW03/homework_group_by.sql
@@ -0,0 +1,91 @@
+/*
+Домашнее задание по курсу MS SQL Server Developer в OTUS.
+Занятие "02 - Оператор SELECT и простые фильтры, GROUP BY, HAVING".
+
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+Нужен WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+
+/*
+1. Посчитать среднюю цену товара, общую сумму продажи по месяцам.
+Вывести:
+* Год продажи (например, 2015)
+* Месяц продажи (например, 4)
+* Средняя цена за месяц по всем товарам
+* Общая сумма продаж за месяц
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
+
+select
+ datepart(YEAR, SCT.FinalizationDate) as 'Год продажи'
+,datepart(MONTH, SCT.FinalizationDate) as 'Месяц продажи'
+,avg (SCT.TransactionAmount) as 'Средняя цена за месяц по всем товарам'
+,sum (SCT.TransactionAmount) as 'Общая сумма продаж за месяц'
+From Sales.CustomerTransactions as SCT
+where SCT.IsFinalized = 1
+group by
+ datepart(YEAR, SCT.FinalizationDate)
+,datepart(MONTH, SCT.FinalizationDate)
+ 
+select * from Sales.Invoices
+where InvoiceID = 5
+select * from Sales.InvoiceLines
+where InvoiceID = 5
+select * from Sales.CustomerTransactions
+where InvoiceID = 5
+select * from Warehouse.StockItemTransactions
+select * from Warehouse.StockItems
+
+/*
+2. Отобразить все месяцы, где общая сумма продаж превысила 4 600 000
+
+Вывести:
+* Год продажи (например, 2015)
+* Месяц продажи (например, 4)
+* Общая сумма продаж
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
+
+напишите здесь свое решение
+
+/*
+3. Вывести сумму продаж, дату первой продажи
+и количество проданного по месяцам, по товарам,
+продажи которых менее 50 ед в месяц.
+Группировка должна быть по году,  месяцу, товару.
+
+Вывести:
+* Год продажи
+* Месяц продажи
+* Наименование товара
+* Сумма продаж
+* Дата первой продажи
+* Количество проданного
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
+
+напишите здесь свое решение
+
+-- ---------------------------------------------------------------------------
+-- Опционально
+-- ---------------------------------------------------------------------------
+/*
+Написать запросы 2-3 так, чтобы если в каком-то месяце не было продаж,
+то этот месяц также отображался бы в результатах, но там были нули.
+*/
