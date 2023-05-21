de493cbf908502b2d210f5165c9c57b0d753dfae
sorokin-va <joker.sva@gmail.com>
2023-05-21 20:31:22 +0300

    new update

--- HW03/homework_group_by.sql
+++ HW03/homework_group_by.sql
@@ -31,24 +31,17 @@ USE WideWorldImporters
 */
 
 select
- datepart(YEAR, SCT.FinalizationDate) as 'Год продажи'
-,datepart(MONTH, SCT.FinalizationDate) as 'Месяц продажи'
-,avg (SCT.TransactionAmount) as 'Средняя цена за месяц по всем товарам'
-,sum (SCT.TransactionAmount) as 'Общая сумма продаж за месяц'
-From Sales.CustomerTransactions as SCT
-where SCT.IsFinalized = 1
+	 datepart(YEAR, FinalizationDate)                 as 'Год продажи'
+	,datepart(MONTH, FinalizationDate)                as 'Месяц продажи'
+	,cast(avg (TransactionAmount) as decimal (10,2))  as 'Средняя цена за месяц по всем товарам'
+	,sum (TransactionAmount)                          as 'Общая сумма продаж за месяц'
+from Sales.CustomerTransactions
+where IsFinalized = 1                   -- под продажами я понимаю завершённые сделки, т.е. с флагом IsFinalized = 1, поэтому добавил это условие.
+	and InvoiceID is not null           -- объясните почему без этого условия агрегаты не работают?
 group by
- datepart(YEAR, SCT.FinalizationDate)
-,datepart(MONTH, SCT.FinalizationDate)
- 
-select * from Sales.Invoices
-where InvoiceID = 5
-select * from Sales.InvoiceLines
-where InvoiceID = 5
-select * from Sales.CustomerTransactions
-where InvoiceID = 5
-select * from Warehouse.StockItemTransactions
-select * from Warehouse.StockItems
+	 datepart(YEAR, FinalizationDate)
+	,datepart(MONTH, FinalizationDate)
+order by 1,2
 
 /*
 2. Отобразить все месяцы, где общая сумма продаж превысила 4 600 000
@@ -61,7 +54,18 @@ select * from Warehouse.StockItems
 Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
 */
 
-напишите здесь свое решение
+select
+	 datepart(YEAR, FinalizationDate)       as 'Год продажи'
+	,datepart(MONTH, FinalizationDate)      as 'Месяц продажи'
+	,sum (TransactionAmount)                as 'Общая сумма продаж за месяц'
+From Sales.CustomerTransactions
+where IsFinalized = 1                   -- под продажами я понимаю завершённые сделки, т.е. с флагом IsFinalized = 1, поэтому добавил это условие.
+	and InvoiceID is not null           -- объясните почему без этого условия агрегаты не работают?
+group by
+	 datepart(YEAR, FinalizationDate)
+	,datepart(MONTH, FinalizationDate)
+having sum (TransactionAmount) > 4600000
+order by 1,2
 
 /*
 3. Вывести сумму продаж, дату первой продажи
@@ -80,7 +84,23 @@ select * from Warehouse.StockItems
 Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
 */
 
-напишите здесь свое решение
+select
+	 datepart(YEAR, SCT.FinalizationDate)   as 'Год продажи'
+	,datepart(MONTH, SCT.FinalizationDate)  as 'Месяц продажи'
+	,WSI.StockItemName                      as 'Наименование товара'
+	,sum (SCT.TransactionAmount)            as 'Сумма продаж'
+	,min (SCT.FinalizationDate)             as 'Дата первой продажи'
+	,count (SIL.StockItemID)                as 'Количество проданного'
+from Sales.InvoiceLines as SIL
+	join Sales.CustomerTransactions as SCT on SIL.InvoiceID = SCT.InvoiceID
+	join Warehouse.StockItems as WSI on SIL.StockItemID = WSI.StockItemID
+where SCT.IsFinalized = 1               -- под продажами я понимаю завершённые сделки, т.е. с флагом IsFinalized = 1, поэтому добавил это условие. (оно же отсекает в результатах NULL'ы по году и месяцу)
+group by 
+	 datepart(YEAR, SCT.FinalizationDate)
+	,datepart(MONTH, SCT.FinalizationDate)
+	,WSI.StockItemName
+having count (SIL.StockItemID) < 50
+order by 1,2,3
 
 -- ---------------------------------------------------------------------------
 -- Опционально
@@ -89,3 +109,54 @@ select * from Warehouse.StockItems
 Написать запросы 2-3 так, чтобы если в каком-то месяце не было продаж,
 то этот месяц также отображался бы в результатах, но там были нули.
 */
+
+-- опционально 2 задание
+select
+	 datepart(YEAR, FinalizationDate)                        as 'Год продажи'
+	,datepart(MONTH, FinalizationDate)                       as 'Месяц продажи'
+	,case
+		when sum(TransactionAmount) > 4600000 then sum(TransactionAmount)
+		else 0
+		end                                                  as 'Общая сумма продаж за месяц'
+From Sales.CustomerTransactions
+where IsFinalized = 1                   -- под продажами я понимаю завершённые сделки, т.е. с флагом IsFinalized = 1, поэтому добавил это условие.
+	and InvoiceID is not null           -- объясните почему без этого условия агрегаты не работают?
+group by
+	 datepart(YEAR, FinalizationDate)
+	,datepart(MONTH, FinalizationDate)
+--having sum (TransactionAmount) > 4600000
+order by 1,2
+
+-- опционально 3 задание   177 товар в 04-2016 количество продаж 52 обработать, и заменить ID товара на наименование
+select
+	 BASE.[Год продажи]
+	,BASE.[Месяц продажи]
+	,BASE.[Наименование товара]
+	,isnull(DETAIL.[Сумма продаж],0)  as 'Сумма продаж'
+	,isnull(DETAIL.[Дата первой продажи],' ') as 'Дата первой продажи'
+	,isnull(DETAIL.[Количество проданного],0) as 'Количество проданного' 
+from (select distinct
+			 datepart(YEAR, E.FinalizationDate)   as 'Год продажи'
+			,datepart(MONTH, E.FinalizationDate)  as 'Месяц продажи'
+			,X.StockItemID                        as 'Наименование товара'
+			,left(E.FinalizationDate,7) as r
+			from Sales.CustomerTransactions as E
+				cross apply (select StockItemID from Warehouse.StockItems) X
+			where E.FinalizationDate is not null) BASE
+left join (select
+				 datepart(YEAR, SCT.FinalizationDate)   as 'Год продажи'
+				,datepart(MONTH, SCT.FinalizationDate)  as 'Месяц продажи'
+				,WSI.StockItemID                      as 'Наименование товара'
+				,sum (SCT.TransactionAmount)            as 'Сумма продаж'
+				,min (SCT.FinalizationDate)             as 'Дата первой продажи'
+				,count (SIL.StockItemID)                as 'Количество проданного'
+			from Sales.InvoiceLines as SIL
+				join Sales.CustomerTransactions as SCT on SIL.InvoiceID = SCT.InvoiceID
+				join Warehouse.StockItems as WSI on SIL.StockItemID = WSI.StockItemID
+			where SCT.IsFinalized = 1               -- под продажами я понимаю завершённые сделки, т.е. с флагом IsFinalized = 1, поэтому добавил это условие. (оно же отсекает в результатах NULL'ы по году и месяцу)
+			group by 
+				 datepart(YEAR, SCT.FinalizationDate)
+				,datepart(MONTH, SCT.FinalizationDate)
+				,WSI.StockItemID
+			having count (SIL.StockItemID) < 50) DETAIL on BASE.[Месяц продажи] = DETAIL.[Месяц продажи] and BASE.[Год продажи] = DETAIL.[Год продажи] and BASE.[Наименование товара] = DETAIL.[Наименование товара]
+order by 1,2,3
