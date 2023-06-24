b3f90f589dd6e41ed8858255776964265d7eecdc
sorokin-va <joker.sva@gmail.com>
2023-06-24 23:47:48 +0300

    new update

--- HW05/homework_window_functions.sql
+++ HW05/homework_window_functions.sql
@@ -120,49 +120,125 @@ order by YEAR_MONTH, TOTAL
 Для этой задачи НЕ нужно писать аналог без аналитических функций.
 */
 -- пронумеруйте записи по названию товара, так чтобы при изменении буквы алфавита нумерация начиналась заново
-select StockItemID, StockItemName, Brand, UnitPrice, ROW_NUMBER () OVER (PARTITION BY left(StockItemName,1) order by StockItemName) as 'нумерация' from Warehouse.StockItems
+select
+ StockItemID
+,StockItemName
+,Brand
+,UnitPrice
+,ROW_NUMBER () OVER (PARTITION BY left(StockItemName,1) order by StockItemName) as 'нумерация'
+from Warehouse.StockItems
 --!!!не совсем понятно условие "при изменении буквы алфавита нумерация начиналась заново"
 
 
 -- посчитайте общее количество товаров и выведете полем в этом же запросе
-select StockItemID, StockItemName, Brand, UnitPrice, ROW_NUMBER () OVER (PARTITION BY left(StockItemName,1) order by StockItemName), sum (QuantityPerOuter) over () as 'общее количество товаров' from Warehouse.StockItems
+select
+ StockItemID
+,StockItemName
+,Brand
+,UnitPrice
+,ROW_NUMBER () OVER (PARTITION BY left(StockItemName,1) order by StockItemName), sum (QuantityPerOuter) over () as 'общее количество товаров'
+from Warehouse.StockItems
 
 
 -- посчитайте общее количество товаров в зависимости от первой буквы названия товара
-select StockItemID, StockItemName, Brand, UnitPrice, ROW_NUMBER () OVER (PARTITION BY left(StockItemName,1) order by StockItemName), sum (QuantityPerOuter) over () as 'общее количество товаров', sum (QuantityPerOuter) over (PARTITION BY left(StockItemName,1) order by left(StockItemName,1)) as 'общее количество товаров по первой букве' from Warehouse.StockItems
+select
+ StockItemID
+,StockItemName
+,Brand
+,UnitPrice
+,ROW_NUMBER () OVER (PARTITION BY left(StockItemName,1) order by StockItemName)
+,sum (QuantityPerOuter) over () as 'общее количество товаров'
+,sum (QuantityPerOuter) over (PARTITION BY left(StockItemName,1) order by left(StockItemName,1)) as 'общее количество товаров по первой букве'
+from Warehouse.StockItems
 
 
 -- отобразите следующий id товара исходя из того, что порядок отображения товаров по имени
-select StockItemID, StockItemName, Brand, UnitPrice, lead(StockItemID) OVER (order by StockItemName) as 'следующий ID' from Warehouse.StockItems
+select
+ StockItemID
+,StockItemName
+,Brand
+,UnitPrice
+,lead(StockItemID) OVER (order by StockItemName) as 'следующий ID'
+from Warehouse.StockItems
 
 
 -- предыдущий ид товара с тем же порядком отображения (по имени)
-select StockItemID, StockItemName, Brand, UnitPrice, lag(StockItemID) OVER (order by StockItemName) as 'предыдыщий ID' from Warehouse.StockItems
+select
+ StockItemID
+,StockItemName
+,Brand
+,UnitPrice
+,lag(StockItemID) OVER (order by StockItemName) as 'предыдыщий ID'
+from Warehouse.StockItems
 
 
 -- названия товара 2 строки назад, в случае если предыдущей строки нет нужно вывести "No items"
-select StockItemID, StockItemName, Brand, UnitPrice, isnull(lag(StockItemName,2) OVER (order by StockItemName),'No items') as 'Предыдущее название две строки назад'
+select
+ StockItemID
+,StockItemName
+,Brand
+,UnitPrice
+,isnull(lag(StockItemName,2) OVER (order by StockItemName),'No items') as 'Предыдущее название две строки назад'
 from Warehouse.StockItems
 
 
 -- сформируйте 30 групп товаров по полю вес товара на 1 шт
-select StockItemID, StockItemName, Brand, UnitPrice, TypicalWeightPerUnit, ntile(30) OVER (PARTITION BY TypicalWeightPerUnit order by TypicalWeightPerUnit) as 'Группа товаров по весу'
+select
+ StockItemID
+,StockItemName
+,Brand
+,UnitPrice
+,TypicalWeightPerUnit
+,ntile(30) OVER (PARTITION BY TypicalWeightPerUnit order by TypicalWeightPerUnit) as 'Группа товаров по весу'
 from Warehouse.StockItems
-order by ntile(30) OVER (PARTITION BY TypicalWeightPerUnit order by TypicalWeightPerUnit)
+order by [Группа товаров по весу]
 -- !! также не совсем понятно условие "по полю вес товара на 1 шт"
 
 /*
 5. По каждому сотруднику выведите последнего клиента, которому сотрудник что-то продал.
    В результатах должны быть ид и фамилия сотрудника, ид и название клиента, дата продажи, сумму сделки.
 */
+; with CTE as (
+				select distinct
+				 SalespersonPersonID
+				,max (InvoiceID) OVER (PARTITION BY SalespersonPersonID) as 'последняя продажа сотрудника'
+				from Sales.Invoices)
 
-напишите здесь свое решение
+select 
+ CTE.SalespersonPersonID
+,AP.FullName
+,SCT.CustomerID
+,SC.CustomerName
+,SCT.TransactionDate
+,SCT.TransactionAmount
+from CTE
+	join Sales.CustomerTransactions as SCT on SCT.InvoiceID = CTE.[последняя продажа сотрудника]
+	join Sales.Customers as SC on SC.CustomerID = SCT.CustomerID
+	join Application.People as AP on AP.PersonID = CTE.SalespersonPersonID
+order by 1, 3   
+
+
 
 /*
 6. Выберите по каждому клиенту два самых дорогих товара, которые он покупал.
 В результатах должно быть ид клиента, его название, ид товара, цена, дата покупки.
 */
-
-напишите здесь свое решение
-
-Опционально можете для каждого запроса без оконных функций сделать вариант запросов с оконными функциями и сравнить их производительность. 
+; with CTE as (
+				select distinct
+				 SO.CustomerID
+				,SOL.UnitPrice
+				,SOL.StockItemID
+				,max(SO.OrderDate) OVER (PARTITION BY SO.CustomerID, SOL.UnitPrice, SOL.StockItemID order by SOL.UnitPrice desc) as 'самая актуальная дата покупки'
+				,DENSE_RANK() OVER (PARTITION BY SO.CustomerID order by SOL.UnitPrice desc) as SORT
+				from Sales.OrderLines as SOL
+					join Sales.Orders as SO on SO.OrderID = SOL.OrderID)
+select 
+ CTE.CustomerID
+,SC.CustomerName
+,CTE.StockItemID
+,CTE.UnitPrice
+,CTE.[самая актуальная дата покупки]
+from CTE
+	join Sales.Customers as SC on SC.CustomerID = CTE.CustomerID
+where CTE.SORT <= 2
+order by 1 asc, 4 desc
